cc32a14ec91547e1c94ac06e2bdec0f3bf3bf9d2
chengshiwen <chengshiwen0103@gmail.com>
2018-08-10 02:25:05 +0800

    fix bug: not setting advertised.listeners

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -2,10 +2,13 @@
 
 KAFKA_ZK_DIR=${KAFKA_ZK_DIR:-/data/zookeeper}
 KAFKA_LOG_DIR=${KAFKA_LOG_DIR:-/data/kafka}
+KAFKA_AD_HOST=${KAFKA_AD_HOST:-localhost}
+KAFKA_AD_PORT=${KAFKA_AD_PORT:-9092}
 
 mkdir -p ${KAFKA_ZK_DIR} ${KAFKA_LOG_DIR}
 
 sed -i "s#dataDir=.*#dataDir=${KAFKA_ZK_DIR}#" ${KAFKA_HOME}/config/zookeeper.properties
 sed -i "s#log.dirs=.*#log.dirs=${KAFKA_LOG_DIR}#" ${KAFKA_HOME}/config/server.properties
+sed -i "/advertised.listeners=PLAINTEXT/a\advertised.listeners=PLAINTEXT://${KAFKA_AD_HOST}:${KAFKA_AD_PORT}" ${KAFKA_HOME}/config/server.properties
 
 supervisord -n $@
