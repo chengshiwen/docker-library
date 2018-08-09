b3d63d3dcaa5889d611a8dd16d906fafea49e00e
chengshiwen <chengshiwen0103@gmail.com>
2018-08-09 23:31:55 +0800

    modify build.sh and README.md

--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Docker library in Common.
 ### Docker library list
 
 - alpine-java
+- kafka
 - ncftp
 - redis-cluster
+
+### Build docker image
+
+    Usage: ./build.sh <library>
--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ -z "$1" ]]; then
-    echo "Usage: $0 <name>"
+    echo "Usage: $0 <library>"
     exit 1
 fi
 
 BUILD_DIR=$(dirname $0)/$1
 
 if [[ ! -d ${BUILD_DIR} ]]; then
-    echo "$1 not exist!"
+    echo "library $1 not exist!"
     exit 1
 fi
 
