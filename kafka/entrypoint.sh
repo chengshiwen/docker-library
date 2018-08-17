a513c7bcd6fa716d640280001bd1a643ed28b073
chengshiwen <chengshiwen0103@gmail.com>
2018-08-17 20:11:28 +0800

    调整kafka镜像

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ "${1#-}" != "$1" ]]; then
+    set -- supervisord -n "$@"
+fi
+
 KAFKA_ZK_DIR=${KAFKA_ZK_DIR:-/data/zookeeper}
 KAFKA_LOG_DIR=${KAFKA_LOG_DIR:-/data/kafka}
 KAFKA_NUM_PARTITIONS=${KAFKA_NUM_PARTITIONS:-1}
@@ -13,4 +17,4 @@ sed -i "s#log.dirs=.*#log.dirs=${KAFKA_LOG_DIR}#" ${KAFKA_HOME}/config/server.pr
 sed -i "s#num.partitions=.*#num.partitions=${KAFKA_NUM_PARTITIONS}#" ${KAFKA_HOME}/config/server.properties
 sed -i "/advertised.listeners=PLAINTEXT/a\advertised.listeners=PLAINTEXT://${KAFKA_ADVERTISED_HOST}:${KAFKA_ADVERTISED_PORT}" ${KAFKA_HOME}/config/server.properties
 
-supervisord -n $@
+exec "$@"
