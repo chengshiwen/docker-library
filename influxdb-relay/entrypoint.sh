e3df1ea283204c398a64a6560858a94c87cde0f9
chengshiwen <chengshiwen0103@gmail.com>
2018-08-18 15:22:38 +0800

    在influxdb-relay中添加环境变量RELAY_HTTP_TIMEOUT、RELAY_BUFFER_SIZE_MB、RELAY_MAX_BATCH_KB、RELAY_MAX_DELAY_INTERVAL

--- influxdb-relay/entrypoint.sh
+++ influxdb-relay/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+RELAY_HTTP_TIMEOUT=${RELAY_HTTP_TIMEOUT:-10s}
+RELAY_BUFFER_SIZE_MB=${RELAY_BUFFER_SIZE_MB:-256}
+RELAY_MAX_BATCH_KB=${RELAY_MAX_BATCH_KB:-512}
+RELAY_MAX_DELAY_INTERVAL=${RELAY_MAX_DELAY_INTERVAL:-10s}
+
 for influxdb in $(env | grep "^INFLUXDB_"); do
     name=$(echo $influxdb | cut -d= -f1)
     addr=$(echo $influxdb | cut -d= -f2)
-    sed -i "/127.0.0.1:7086/a\    { name=\"${name#INFLUXDB_}\", location=\"http://$addr/write\", timeout=\"10s\" }," /etc/relay.toml
+    sed -i "/127.0.0.1:7086/a\    { name=\"${name#INFLUXDB_}\", location=\"http://$addr/write\", timeout=\"${RELAY_HTTP_TIMEOUT}\", buffer-size-mb=${RELAY_BUFFER_SIZE_MB}, max-batch-kb=${RELAY_MAX_BATCH_KB}, max-delay-interval=\"${RELAY_MAX_DELAY_INTERVAL}\" }," /etc/relay.toml
     # sed -i "/127.0.0.1:7089/a\    { name=\"${name#INFLUXDB_}\", location=\"${addr%:*}:8089\", mtu=512 }," /etc/relay.toml
 done
 
